54970c53b7a162fd0bc652f8ea10995b50f71499
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2021-09-08 09:24:33 +0200

    feat(patch): implemented changes from PoC done by @knight42

--- staging/src/k8s.io/cli-runtime/pkg/genericclioptions/client_config.go
+++ staging/src/k8s.io/cli-runtime/pkg/genericclioptions/client_config.go
@@ -38,9 +38,14 @@ var _ = clientcmd.ClientConfig(&clientConfig{})
 
 type clientConfig struct {
 	defaultClientConfig clientcmd.ClientConfig
+	local               bool
 }
 
 func (c *clientConfig) RawConfig() (clientcmdapi.Config, error) {
+	if c.local {
+		return clientcmdapi.Config{}, nil
+	}
+
 	config, err := c.defaultClientConfig.RawConfig()
 	// replace client-go's ErrEmptyConfig error with our custom, more verbose version
 	if clientcmd.IsEmptyConfig(err) {
@@ -50,6 +55,10 @@ func (c *clientConfig) RawConfig() (clientcmdapi.Config, error) {
 }
 
 func (c *clientConfig) ClientConfig() (*restclient.Config, error) {
+	if c.local {
+		return &restclient.Config{}, nil
+	}
+
 	config, err := c.defaultClientConfig.ClientConfig()
 	// replace client-go's ErrEmptyConfig error with our custom, more verbose version
 	if clientcmd.IsEmptyConfig(err) {
@@ -60,6 +69,10 @@ func (c *clientConfig) ClientConfig() (*restclient.Config, error) {
 
 func (c *clientConfig) Namespace() (string, bool, error) {
 	namespace, ok, err := c.defaultClientConfig.Namespace()
+	if c.local {
+		return namespace, ok, nil
+	}
+
 	// replace client-go's ErrEmptyConfig error with our custom, more verbose version
 	if clientcmd.IsEmptyConfig(err) {
 		return namespace, ok, ErrEmptyConfig
